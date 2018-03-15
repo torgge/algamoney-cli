a49fb0f162a5026d4cb43003c0c174d88ff583ab
TOSHI-BONESPIRI\torgg <torgge@gmail.com>
2018-03-15 06:20:44 -0300

    13.8. Validando controles de formulário com PrimeNG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import {InputTextModule} from 'primeng/components/inputtext/inputtext';
 import { AppComponent } from './app.component';
+import {NavbarComponent} from './navbar/navbar.component';
+
+import {CurrencyMaskModule} from 'ng2-currency-mask';
+
+import {PessoaCadastroComponent} from './pessoa-cadastro/pessoa-cadastro.component';
+import {LancamentosPesquisaComponent} from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+import {PessoasPesquisaComponent} from './pessoas-pesquisa/pessoas-pesquisa.component';
+import {LancamentoCadastroComponent} from './lancamento-cadastro/lancamento-cadastro.component';
+
 import {ButtonModule} from 'primeng/components/button/button';
 import {PaginatorModule} from 'primeng/components/paginator/paginator';
 import {DataTableModule} from 'primeng/components/datatable/datatable';
 import {TooltipModule} from 'primeng/components/tooltip/tooltip';
 import {InputTextareaModule} from 'primeng/components/inputtextarea/inputtextarea';
-import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
-import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
+import {InputTextModule} from 'primeng/components/inputtext/inputtext';
 import {CalendarModule} from 'primeng/components/calendar/calendar';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {SelectButtonModule} from 'primeng/components/selectbutton/selectbutton';
-import {CurrencyMaskModule} from 'ng2-currency-mask';
-import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
 import {InputMaskModule} from 'primeng/components/inputmask/inputmask';
-
 
 @NgModule({
   declarations: [
@@ -41,7 +44,8 @@ import {InputMaskModule} from 'primeng/components/inputmask/inputmask';
     CalendarModule,
     SelectButtonModule,
     CurrencyMaskModule,
-    InputMaskModule
+    InputMaskModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
